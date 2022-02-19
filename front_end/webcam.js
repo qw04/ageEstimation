0e30eee3feb135c015ed2e2e55c6cb5629de3856
qw04 <shahsaumya2004@gmail.com>
2022-02-19 18:40:23 +0000

    some changes

--- front_end/webcam.js
+++ front_end/webcam.js
@@ -1,11 +1,26 @@
 var video = document.querySelector("#videoElement");
 
-if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-      video.srcObject = stream;
-    })
-    .catch(function (err0r) {
-      console.log("Something went wrong!");
-    });
-}
+// if (navigator.mediaDevices.getUserMedia) {
+//   navigator.mediaDevices.getUserMedia({ video: true })
+
+//       .then(function (stream) {
+//       video.srcObject = stream;
+//     }).catch(function (err0r) {
+//       alert("Something went wrong, reload webpage!");
+//     });
+// }
+
+
+navigator.mediaDevices.enumerateDevices()
+.then(function(devices) {
+  array = [];
+  devices.forEach(function(device) {
+    if (device.kind === "videoinput"){
+      array.push([device.deviceId,device.label]);
+    };
+    console.log(array);
+  }
+  )
+}).catch(function(err) {
+  alert(err.name + ": " + err.message);
+});
