882f064b558bbf7a419683dd056452fe379a4f54
qw04 <shahsaumya2004@gmail.com>
2022-03-29 23:04:56 +0100

    got rid of settings

--- front_end/settings.js
+++
@@ -1,7 +0,0 @@
-boundaryRequest = document.querySelector('#boundaryRequest');
-for (var i = 1; i <= 110; i++) {
-  const option = document.createElement('option');
-  option.value = i;
-  option.text = i;
-  boundaryRequest.appendChild(option);
-}
